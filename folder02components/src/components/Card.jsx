9df775da36ac6533b579087400005c80878baf24
Sudhan06-45 <Sudhan.Suresh@WinWire.com>
2025-11-19 15:42:03 +0530

    components

---
+++ folder02components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h2>Person Details</h2>
+        <p>Name: Sudhan S</p>
+        <p>ID: 3581</p>
+        <p>Course: App Dev</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ folder02components/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
